6eda78d0a4cdec145eefa55422fdbc3467404c7c
Kyle <kyle.muccignat@my.jcu.edu.au>
2020-08-27 14:48:46 +1000

    Add password program separated into functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,20 @@
 MINIMUM_LENGTH = 4
 
-password_input = input("Password: ")
-password_length = len(password_input)
-if password_length < MINIMUM_LENGTH:
+
+def main():
+    get_password()
+
+
+def get_password():
     password_input = input("Password: ")
-print(password_length * "*")
+    password_length = len(password_input)
+    print_asterisks(password_length)
+
+
+def print_asterisks(password_length):
+    if password_length < MINIMUM_LENGTH:
+        password_input = input("Password: ")
+    print(password_length * "*")
+
+
+main()
